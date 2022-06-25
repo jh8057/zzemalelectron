f4b1e188d0400d858b04d22faf66633da07066e0
jh8057 <luckysymbol13@gmail.com>
2022-06-26 00:36:23 +0900

    mocha

---
+++ src/index.js
@@ -0,0 +1,5 @@
+function mochaTest() {
+  return "mochaTest";
+}
+
+export { mochaTest };
---
+++ test/test.js
@@ -0,0 +1,32 @@
+import { strict as assert } from "assert";
+
+describe("Array", function () {
+  describe("#indexOf()", function () {
+    it("should return -1 when the value is not present", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+
+    it("1+1 = 2", () => {
+      assert.equal(1 + 1, 2);
+    });
+
+    it("async - setTimeout & done", function (done) {
+      setTimeout(() => {
+        let a = "setTimeout";
+        assert.equal(a, "setTimeout");
+        done();
+      }, 1000);
+    });
+  });
+});
+
+//////////////// 여기까지 자체 테스트
+// 여기부터 외부 import해보기
+
+import { mochaTest } from "../src/index.js";
+
+describe("mochaTest", () => {
+  it('mochaTest sholud String "mochaTest"', () => {
+    assert.equal(mochaTest(), "mochaTest");
+  });
+});
