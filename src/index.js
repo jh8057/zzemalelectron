0579900e74a8cf2934e8cb51ec0e3ef7cf527fea
jh8057 <luckysymbol13@gmail.com>
2022-07-02 18:48:22 +0900

    slack msg -input setting

--- src/index.js
+++ src/index.js
@@ -6,4 +6,32 @@ function linkTest() {
   return "linked";
 }
 
+function initPage() {
+  //hour
+  let targetBodyH = document.querySelector(".hour");
+  let selecthourH = document.createElement("select");
+  selecthourH.className = "hourSelected";
+  console.log(selecthourH);
+  for (let i = 0; i < 24; i++) {
+    let optionH = document.createElement("option");
+    optionH.value = i;
+    optionH.text = i;
+    selecthourH.add(optionH, null);
+  }
+  targetBodyH.appendChild(selecthourH);
+
+  //minute
+  let targetBodyM = document.querySelector(".minute");
+  let selecthourM = document.createElement("select");
+  selecthourM.className = "minuteSelected";
+  for (let i = 0; i < 60; i++) {
+    let optionM = document.createElement("option");
+    optionM.value = i;
+    optionM.text = i;
+    selecthourM.add(optionM, null);
+  }
+  targetBodyM.appendChild(selecthourM);
+}
+
+initPage();
 // module.exports = { linkTest };
